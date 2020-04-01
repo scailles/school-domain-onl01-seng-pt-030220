02d0376a77f8242fd1b55097439a20f3dfd33833
Shaina C <scailles23@gmail.com>
2020-04-01 08:36:41 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,31 @@
-# code here!
+class School
+  
+  attr_accessor :student, :class, :grade
+  
+  attr_reader :name
+  
+  
+  def initialize(roster)
+    @name=name
+    @roster={}
+  end
+  
+  def roster
+    @roster
+  end
+  
+  def add_student(name, grade)
+   if !(@roster.include?(@roster[grade]))
+     @roster[grade]=grade
+     @roster[grade] << name
+   else 
+     @roster[grade] << name
+   end
+ end
+  
+  def grade(grade)
+    @roster[grade]
+  end
+
+  
+end
