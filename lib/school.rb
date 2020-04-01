74e993a1b255db42ffdeb58523fcf0d74bd67825
Shaina C <scailles23@gmail.com>
2020-04-01 08:39:18 +0000

    Automatically backed up by Learn

--- lib/school.rb
+++ lib/school.rb
@@ -13,7 +13,7 @@ class School
     @roster
   end
   
-  def add_student(name, grade)
+  def add_student(student_name, grade)
     @student_name=student_name
     @grade= grade
    if !(@roster.include?(@roster[grade]))
